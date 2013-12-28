740f609c11f82da3f461eb857a6487173976a48b
Cliff Crosland <cliffcrosland@gmail.com>
2013-12-28 10:48:56 -0800

    Big, medium, small words in separate lists.

--- app-ctrl.js
+++ app-ctrl.js
@@ -3,12 +3,19 @@ angular.module('App', []);
 angular.module('App')
 .controller('AppCtrl', ['$scope', 'lexiconService', function ($scope, lexiconService) {
   $scope.letters = '';
-  $scope.words = [];
+  $scope.bigWords = [];
+  $scope.mediumWords = [];
+  $scope.smallWords = [];
   $scope.onChange = function () {
     console.log('changed');
     lexiconService.getWords($scope.letters, function (words) {
+      var bigWords = _.filter(words, function (word) { return word.length > 6; });
+      var mediumWords = _.filter(words, function (word) { return word.length <= 6 && word.length > 3; });
+      var smallWords = _.filter(words, function (word) { return word.length <= 3; });
       $scope.$apply(function () {
-        $scope.words = words;
+        $scope.bigWords = bigWords;
+        $scope.mediumWords = mediumWords;
+        $scope.smallWords = smallWords;
       });
     });
   };
